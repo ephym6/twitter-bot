916609bac01785e0cd3bd7aa660856e1e8ed4ed4
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-07-22 20:15:52 +0300

    used dotenv to store credentials

--- tweepy_twitter_bot.py
+++ tweepy_twitter_bot.py
@@ -1,2 +1,14 @@
+import os
+from dotenv import load_dotenv
 import tweepy as tw
-import tkinter as tk
+import tkinter as tk # for GUI interface
+
+# Load environment variables from .env file
+load_dotenv()
+
+consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
+consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
+access_token = os.getenv('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+
+
