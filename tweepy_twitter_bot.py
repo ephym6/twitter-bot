831de6f7597e883c5c0836cedfd310a584674b93
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-07-22 20:25:56 +0300

    Authenticated account with tweepy

--- tweepy_twitter_bot.py
+++ tweepy_twitter_bot.py
@@ -11,4 +11,12 @@ consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
 access_token = os.getenv('TWITTER_ACCESS_TOKEN')
 access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 
+# Authenticate account with tweepy
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth)
+
+# Check if program is working
+user = api.verify_credentials()
+print(user.name)
 
